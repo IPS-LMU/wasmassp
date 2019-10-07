6a2a6e6a2ad5ac2818a9c4543cf2a15ec5170cd2
Raphael Winkelmann <raphael@phonetik.uni-muenchen.de>
2019-10-07 18:37:48 +0200

    updated build

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ source ../emsdk/emsdk_env.sh
 emcc -O3 -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' \
     -I assp \
     -I cJSON/build/temp/usr/local/include \
+    -L cJSON/build/temp/usr/local/lib \
     performAssp.c \
     assp/*.c \
-    -l libcjson \
     --emrun
